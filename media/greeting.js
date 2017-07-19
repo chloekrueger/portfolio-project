3de6151084824133435eef0181e64298c5e108a9
Chloe <chloe.e.krueger@gmail.com>
2017-07-18 20:13:53 -0400

    Added JS to media page

---
+++ media/greeting.js
@@ -0,0 +1,3 @@
+var name = prompt("Hi there! What's your name?");
+var output = document.querySelector('#greeting');
+output.innerHTML = "<p>Thanks for visiting, " + name + ". Enjoy these sweet vids!</p>";
