0ccec2a93a391445d0fcd82fe1cdc07a50aec7e8
Chloe <chloe.e.krueger@gmail.com>
2017-08-14 18:49:53 -0400

    trying to resolve issues

--- effects.js
+++ effects.js
@@ -1,10 +1,3 @@
-/*$('li').on('mouseover', function(){
-  $(this).addClass('highlighted')
-})
-$('li').on('mouseleave', function(){
-  $(this).removeClass('highlighted')
-})*/
-
 $('.photo').on('click', function(){
   $('#hidden-blurb').slideDown(); // only works if #hidden-blurb has display:none; in its CSS
 });
